6e1af1dccba1219813284a9e3011e04ebef318ab
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-07-20 16:04:43 +0300

    updates

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,  include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),
-    path('bookshelf/', include('bookshelf.urls')),
+    path('', include('relationship_app.urls')),
+   
 ]
--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
     
-
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
