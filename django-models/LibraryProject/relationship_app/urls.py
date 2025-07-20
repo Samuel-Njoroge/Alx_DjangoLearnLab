d71019a950db70b66102358689160451e2052ccd
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-07-20 22:05:46 +0300

    permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import admin_view, librarian_view, member_view, list_books
+from .views import admin_view, librarian_view, member_view
+from .views import list_books
 from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
