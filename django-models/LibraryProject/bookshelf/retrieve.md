0986cc080ff7c339931fbab895fb7d6216515955
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-07-20 12:30:09 +0300

    feat: Model Relationship

---
+++ django-models/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,10 @@
+## Retrieve the Book.
+
+```python
+book = Book.objects.get(title="1984")
+book.title  # '1984'
+book.author  # 'George Orwell'
+book.publication_year  # 1949
+```
+
+---
---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return super().__str__()
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+
+    def __str__(self):
+        return super().__str__()
+    
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return super().__str__()
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return super().__str__()
+
+    
