12c16f8b5c986fe4d3b8c52031baa0d07872c974
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-07-20 22:08:16 +0300

    updates done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,7 +15,7 @@ from django import forms
 # User Registration
 class RegisterView(FormView):
     template_name = 'relationship_app/register.html'
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
