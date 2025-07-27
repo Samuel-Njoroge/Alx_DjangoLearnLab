a9eb804efa9c9a36f9e47c97ba7362cb78ae8865
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-07-27 14:07:59 +0300

    advanced security

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,14 +1,3 @@
-# from django.db import models
-
-
-# class Book(models.Model):
-#     title = models.CharField(max_length=200)
-#     author = models.CharField(max_length=100)
-#     publication_year = models.IntegerField()
-
-#     def __str__(self):
-#         return f"{self.title} by {self.author} ({self.publication_year})"
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
@@ -98,3 +87,12 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField()
+
+#     def __str__(self):
+#         return f"{self.title} by {self.author} ({self.publication_year})"
