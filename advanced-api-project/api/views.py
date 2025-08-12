b4865a83a740f00e4eac958d7dd57678a2a4773b
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-12 23:53:36 +0300

    Filtering, Searching, Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework import viewsets, permissions, filters 
 from rest_framework import generics
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
