fa1817dcafe1dbbd4f5f2055267c60055b77a999
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-17 14:37:49 +0300

    feat: Tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 
 # User Creation Form
@@ -19,12 +20,13 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            "tags": TagWidget(),
+        }
         
 # Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-        widgets = {
-            "content": forms.Textarea(attrs={"rows": 3, "placeholder": "Write your comment here..."})
-        }
+        
