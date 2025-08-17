9487ae29306fde03b8f4058bf22810947e6cc8cc
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-17 13:38:09 +0300

    feat: Comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, register, profile, UserLoginView, UserLogoutView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, add_comment, CommentUpdateView, CommentDeleteView
+from .views import home, register, profile, UserLoginView, UserLogoutView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path("posts/<int:post_id>/comments/new/", add_comment, name="add-comment"),
+    path("posts/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="add-comment"),
     path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="edit-comment"),
     path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="delete-comment"),
     path("register/", register, name="register"),
