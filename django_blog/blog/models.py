a56cf9dd63d611f7f0327f2e3498d25b0f93a650
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-17 11:18:53 +0300

    feat: blog setup

---
+++ django_blog/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Post
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+
+
+    def __str__(self):
+        return self.title
