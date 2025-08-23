46ae2b075809897fbd8c465cef4a39eaf7abc9ef
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-23 22:04:35 +0300

    set up and auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,14 +13,14 @@ class UserSerializer(serializers.ModelSerializer):
 
 # Register
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User 
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
